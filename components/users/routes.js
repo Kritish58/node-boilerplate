362d4a3551d604d489418c14dc3c166b35dc6532
kritish bhattarai <rishi58rishi@gmail.com>
2021-02-19 00:06:57 +0545

    love service pattern

--- components/users/routes.js
+++ components/users/routes.js
@@ -3,5 +3,6 @@ const Controllers = require('./controllers');
 const router = express.Router();
 
 router.post('/signup', Controllers.signup);
+router.post('/login', Controllers.login);
 
 module.exports = router;
